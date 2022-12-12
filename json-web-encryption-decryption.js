cee773461d1de8134bcf37b900bca1fff808fe3f
vishwas1 <vishwasanand.cse@gmail.com>
2022-12-12 11:40:26 +0530

    updated

--- json-web-encryption-decryption.js
+++ json-web-encryption-decryption.js
@@ -1,6 +1,7 @@
 import { Cipher } from '@digitalbazaar/minimal-cipher';
 import { X25519KeyAgreementKey2020 } from '@digitalbazaar/x25519-key-agreement-key-2020';
 import { getEd25519KeyPair } from './ed25519KeyPair.js';
+import message from './message.js';
 
 async function start(){
   console.log('Preparing X25519KeyAgreementKey2020 from Ed25519VerificationKey2020 ...................');
@@ -21,7 +22,7 @@ async function start(){
   console.log('Ready for encryption and decryption using X25519KeyAgreementKey2020 ...................');
   console.log('Plaing text ......')
   // Plain text object to sign
-  const obj = { key: 'value' };
+  const obj = message;
   console.log(obj)
   const cipher = new Cipher();
 
@@ -34,7 +35,7 @@ async function start(){
       alg: 'ECDH-ES+A256KW' // TODO: Need to find reference 
     }
   }
-  const recipients = [recipient];
+  const recipients = [recipient]; // TODO: Test encryption for multiple parties
  
 
   // A function that returns a Promise that resolves a key ID to a DH public key.
@@ -48,13 +49,13 @@ async function start(){
   }
   
   // Encrypts some data for one or more recipients and outputs a JWE
-  // To encrypt Uint8Array or a string;  use cipher.encrypt() The data to encrypt can be given as a Uint8Array or a string.
-  // cipher.encryptObject Encrypts an object. The object will be serialized to JSON and passed
+  // To encrypt Uint8Array or a string;  use `cipher.encrypt()` The data to encrypt can be given as a Uint8Array or a string.
+  // `cipher.encryptObject` Encrypts an object. The object will be serialized to JSON and passed
   // Doc: https://github.com/digitalbazaar/minimal-cipher/blob/main/lib/Cipher.js#L98
   // options.keyResolver - A function that returns a Promise that resolves a key ID to a DH public key.
   const jweDoc = await cipher.encryptObject({ obj, recipients, keyResolver });
   console.log('Encrypted text (JWE)......')
-  console.log(jweDoc)
+  console.log(JSON.stringify(jweDoc))
 
   const object = await cipher.decryptObject({ jwe: jweDoc, keyAgreementKey });
   console.log('Decrypted text ......')
